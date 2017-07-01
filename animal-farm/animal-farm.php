b92697bc38a8a03d0a7d4baa325665d72ad05848
Pavel Korotenko <pavel@webdevstudios.com>
2017-06-30 23:28:03 -0400

    Add statistics class and method for finding same ids between goats and sheeps.

--- animal-farm/animal-farm.php
+++ animal-farm/animal-farm.php
@@ -103,6 +103,42 @@ class Sheep extends Animal {
 	}
 }
 
+/**
+ * Statistics class.
+ */
+ class Stats {
+
+	/**
+	* Finds equal ids between goats and sheeps arrays.
+	*
+	* @param array Goats ids.
+	* @param array Sheeps ids.
+	*
+	* @return boolean|array An array of equal ids or false if we have no valid data or no matches.
+	*/
+	 public function find_equals( $goats, $sheeps ) {
+
+		// Bail early if no valid data.
+		if ( empty( $goats ) || empty( $sheeps ) || ! is_array( $goats ) || ! is_array( $sheeps ) ) {
+			return false;
+		}
+
+		$soulmates = array_intersect( $goats, $sheeps );
+
+		// Return false if we have no matches.
+		if ( empty( $soulmates ) || ! is_array( $soulmates ) ) {
+			return false;
+		}
+
+		// Save ids to file.
+		foreach( $soulmates as $match ) {
+			$saved = file_put_contents( 'soulmates.txt', $match . ',', FILE_APPEND | LOCK_EX );
+		}
+
+		return $soulmates;
+	 }
+ }
+
 // Generate 100 of goats and 100 of sheeps and save their ids into an respective arrays.
 $goats  = array();
 $sheeps = array();
@@ -122,4 +158,17 @@ for ( $i = 1; $i <= 100; $i++ ) {
 	if ( property_exists( $sheep, 'animal_id' ) ) {
 		$sheeps[] = $sheep->animal_id;
 	}
+}
+
+// Perform our statistics.
+$stats = new Stats();
+
+// Find equals.
+$soulmates = $stats->find_equals( $goats, $sheeps );
+
+// Display message about soulmates.
+if ( ! empty( $soulmates ) ) {
+	echo 'The total number of matches between goats and sheeps: ' . count( $soulmates );
+} else {
+	echo 'There were no matches!';
 }
